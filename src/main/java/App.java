3c5c46dd8a80c50a4accf2890396185a73d21fcb
jocelyn alsdorf & mlmendeznunez & morgan lutz <student@epicodus.com>
2015-08-26 10:46:59 -0700

    add update and delete for tasks

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -65,9 +65,7 @@ public class App {
     //String name = request.queryParams("name");
   //**** instead of declaring category here, you could also just put it in model.put like above
     Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
-
     String description = request.queryParams("description");
-
     Task newTask = new Task(description, category.getId());
     newTask.save();
     model.put("category",category);
@@ -79,7 +77,6 @@ public class App {
   get("categories/:id/update", (request, response) -> {
     HashMap<String, Object> model = new HashMap<String, Object>();
     Category category = Category.find(Integer.parseInt(request.params(":id")));
-
     model.put("category", category);
     model.put("template", "templates/update-form.vtl");
     return new ModelAndView(model, layout);
@@ -90,11 +87,44 @@ public class App {
     Category category = Category.find(Integer.parseInt(request.params(":id")));
     String name = request.queryParams("name");
     category.update(name);
-
-    response.redirect("/");
+    response.redirect("/categories/:id");
     return null;
   });
 
+  post("/:category_id/tasks/:id/delete", (request, response) -> {
+ //need to put both :ids in the url so that we can grab it below but can't be same name as eachother
+   HashMap<String, Object> model = new HashMap<String, Object>();
+ //make category and task available and then find task by its id
+   Category category = Category.find(Integer.parseInt(request.params(":category_id")));
+   Task task = Task.find(Integer.parseInt(request.params(":id")));
+   model.put("template", "templates/index.vtl");
+   task.delete();
+   model.put("categories", Category.all());
+   return new ModelAndView(model, layout);
+ }, new VelocityTemplateEngine());
+ //
+
+ get("/:category_id/tasks/:id/update", (request, response) -> {
+   HashMap<String, Object> model = new HashMap<String, Object>();
+   Category category = Category.find(Integer.parseInt(request.params(":category_id")));
+   Task task = Task.find(Integer.parseInt(request.params(":id"))); //?
+   model.put("category", category);
+   model.put("task", task); //model.put("task", task) not needed for /delete, must add for /update
+   model.put("template", "templates/edit_task.vtl");
+   return new ModelAndView(model, layout);
+ }, new VelocityTemplateEngine());
+
+ post("/:category_id/tasks/:id/update", (request, response) -> {
+   HashMap<String, Object> model = new HashMap<String, Object>();
+   Category category = Category.find(Integer.parseInt(request.params(":category_id")));
+   Task task = Task.find(Integer.parseInt(request.params(":id")));
+
+   String description = request.queryParams("description");
+   task.update(description);
+   //task.save(); //is save built in to update method?
+   response.redirect("/");
+   return null;
+ });
 
 }//end of main
 
